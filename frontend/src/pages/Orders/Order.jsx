e51d9ef5cb1f7af8cbeb38248f0aa095dac9fa3b
Tashika Wijesooriya <it22644244@my.sliit.lk>
2025-03-28 16:37:52 +0530

    Add Order component for displaying individual order details

--- frontend/src/pages/Orders/Order.jsx
+++ frontend/src/pages/Orders/Order.jsx
@@ -3,7 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
-import Messsage from "../../components/Message";
+import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import {
   useDeliverOrderMutation,
@@ -21,14 +21,11 @@ const Order = () => {
     isLoading,
     error,
   } = useGetOrderDetailsQuery(orderId);
-
   const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
   const [deliverOrder, { isLoading: loadingDeliver }] =
     useDeliverOrderMutation();
   const { userInfo } = useSelector((state) => state.auth);
-
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
-
   const {
     data: paypal,
     isLoading: loadingPaPal,
@@ -40,24 +37,18 @@ const Order = () => {
       const loadingPaPalScript = async () => {
         paypalDispatch({
           type: "resetOptions",
-          value: {
-            "client-id": paypal.clientId,
-            currency: "USD",
-          },
+          value: { "client-id": paypal.clientId, currency: "USD" },
         });
         paypalDispatch({ type: "setLoadingStatus", value: "pending" });
       };
-
-      if (order && !order.isPaid) {
-        if (!window.paypal) {
-          loadingPaPalScript();
-        }
+      if (order && !order.isPaid && !window.paypal) {
+        loadingPaPalScript();
       }
     }
   }, [errorPayPal, loadingPaPal, order, paypal, paypalDispatch]);
 
-  function onApprove(data, actions) {
-    return actions.order.capture().then(async function (details) {
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then(async (details) => {
       try {
         await payOrder({ orderId, details });
         refetch();
@@ -66,40 +57,37 @@ const Order = () => {
         toast.error(error?.data?.message || error.message);
       }
     });
-  }
+  };
 
-  function createOrder(data, actions) {
+  const createOrder = (data, actions) => {
     return actions.order
-      .create({
-        purchase_units: [{ amount: { value: order.totalPrice } }],
-      })
-      .then((orderID) => {
-        return orderID;
-      });
-  }
+      .create({ purchase_units: [{ amount: { value: order.totalPrice } }] })
+      .then((orderID) => orderID);
+  };
 
-  function onError(err) {
+  const onError = (err) => {
     toast.error(err.message);
-  }
+  };
 
   const deliverHandler = async () => {
     await deliverOrder(orderId);
     refetch();
   };
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
-    <Messsage variant="danger">{error.data.message}</Messsage>
-  ) : (
-    <div className="container flex flex-col ml-[10rem] md:flex-row">
-      <div className="md:w-2/3 pr-4">
-        <div className="border gray-300 mt-5 pb-4 mb-5">
+  if (isLoading) return <Loader />;
+  if (error) return <Message variant="danger">{error.data.message}</Message>;
+
+  return (
+    <div className="max-w-4xl mx-auto p-6">
+      <div className="bg-white shadow-lg rounded-lg p-6 space-y-6">
+        {/* Cart Details */}
+        <div>
+          <h2 className="text-2xl font-bold mb-4">Order Details</h2>
           {order.orderItems.length === 0 ? (
-            <Messsage>Order is empty</Messsage>
+            <Message>Order is empty</Message>
           ) : (
             <div className="overflow-x-auto">
-              <table className="w-[80%]">
+              <table className="min-w-full table-auto">
                 <thead className="border-b-2">
                   <tr>
                     <th className="p-2">Image</th>
@@ -109,7 +97,6 @@ const Order = () => {
                     <th className="p-2">Total</th>
                   </tr>
                 </thead>
-
                 <tbody>
                   {order.orderItems.map((item, index) => (
                     <tr key={index}>
@@ -120,15 +107,18 @@ const Order = () => {
                           className="w-16 h-16 object-cover"
                         />
                       </td>
-
                       <td className="p-2">
-                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                        <Link
+                          to={`/product/${item.product}`}
+                          className="text-blue-500"
+                        >
+                          {item.name}
+                        </Link>
                       </td>
-
                       <td className="p-2 text-center">{item.qty}</td>
                       <td className="p-2 text-center">{item.price}</td>
                       <td className="p-2 text-center">
-                        $ {(item.qty * item.price).toFixed(2)}
+                        ${(item.qty * item.price).toFixed(2)}
                       </td>
                     </tr>
                   ))}
@@ -137,91 +127,60 @@ const Order = () => {
             </div>
           )}
         </div>
-      </div>
 
-      <div className="md:w-1/3">
-        <div className="mt-5 border-gray-300 pb-4 mb-4">
-          <h2 className="text-xl font-bold mb-2">Shipping</h2>
-          <p className="mb-4 mt-4">
-            <strong className="text-pink-500">Order:</strong> {order._id}
-          </p>
+        {/* Payment Summary */}
+        <div>
+          <h2 className="text-2xl font-bold mb-4">Order Summary</h2>
+          <div className="space-y-4">
+            <div className="flex justify-between">
+              <span>Items</span>
+              <span>${order.itemsPrice}</span>
+            </div>
+            <div className="flex justify-between">
+              <span>Shipping</span>
+              <span>${order.shippingPrice}</span>
+            </div>
+            <div className="flex justify-between">
+              <span>Tax</span>
+              <span>${order.taxPrice}</span>
+            </div>
+            <div className="flex justify-between">
+              <span>Total</span>
+              <span>${order.totalPrice}</span>
+            </div>
 
-          <p className="mb-4">
-            <strong className="text-pink-500">Name:</strong>{" "}
-            {order.user.username}
-          </p>
-
-          <p className="mb-4">
-            <strong className="text-pink-500">Email:</strong> {order.user.email}
-          </p>
-
-          <p className="mb-4">
-            <strong className="text-pink-500">Address:</strong>{" "}
-            {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
-            {order.shippingAddress.postalCode}, {order.shippingAddress.country}
-          </p>
-
-          <p className="mb-4">
-            <strong className="text-pink-500">Method:</strong>{" "}
-            {order.paymentMethod}
-          </p>
-
-          {order.isPaid ? (
-            <Messsage variant="success">Paid on {order.paidAt}</Messsage>
-          ) : (
-            <Messsage variant="danger">Not paid</Messsage>
-          )}
-        </div>
-
-        <h2 className="text-xl font-bold mb-2 mt-[3rem]">Order Summary</h2>
-        <div className="flex justify-between mb-2">
-          <span>Items</span>
-          <span>$ {order.itemsPrice}</span>
-        </div>
-        <div className="flex justify-between mb-2">
-          <span>Shipping</span>
-          <span>$ {order.shippingPrice}</span>
-        </div>
-        <div className="flex justify-between mb-2">
-          <span>Tax</span>
-          <span>$ {order.taxPrice}</span>
-        </div>
-        <div className="flex justify-between mb-2">
-          <span>Total</span>
-          <span>$ {order.totalPrice}</span>
-        </div>
-
-        {!order.isPaid && (
-          <div>
-            {loadingPay && <Loader />}{" "}
-            {isPending ? (
-              <Loader />
-            ) : (
+            {!order.isPaid && (
               <div>
-                <div>
+                {loadingPay && <Loader />}
+                {isPending ? (
+                  <Loader />
+                ) : (
                   <PayPalButtons
                     createOrder={createOrder}
                     onApprove={onApprove}
                     onError={onError}
-                  ></PayPalButtons>
-                </div>
+                  />
+                )}
               </div>
             )}
-          </div>
-        )}
 
-        {loadingDeliver && <Loader />}
-        {userInfo && userInfo.isAdmin && order.isPaid && !order.isDelivered && (
-          <div>
-            <button
-              type="button"
-              className="bg-pink-500 text-white w-full py-2"
-              onClick={deliverHandler}
-            >
-              Mark As Delivered
-            </button>
+            {loadingDeliver && <Loader />}
+            {userInfo &&
+              userInfo.isAdmin &&
+              order.isPaid &&
+              !order.isDelivered && (
+                <div>
+                  <button
+                    type="button"
+                    className="bg-pink-500 text-white w-full py-2 rounded-md"
+                    onClick={deliverHandler}
+                  >
+                    Mark As Delivered
+                  </button>
+                </div>
+              )}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
